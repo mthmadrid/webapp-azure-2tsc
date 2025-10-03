589baacdd3a295de5a058756ffe8a7db11d52c06
Matheus Madrid <teteumadrid@gmail.com>
2025-10-03 00:03:30 +0000

    Passo 5: Corrige nome do resource type no mysql.tf

--- mysql.tf
+++ mysql.tf
@@ -36,7 +36,7 @@ resource "azurerm_mysql_flexible_server_firewall_rule" "allow_azure_services" {
   end_ip_address      = "0.0.0.0"
 }
 
-resource "azurerm_mysql_flexible_server_database" "mysql_database_two" {
+resource "azurerm_mysql_flexible_database" "mysql_database_two" {
   name                = var.mysql_database_two
   resource_group_name = azurerm_resource_group.rg.name
   server_name         = azurerm_mysql_flexible_server.mysql.name
